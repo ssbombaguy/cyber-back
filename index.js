826407bed07f949f487fe29d6b19191e861c54b5
ssbombaguy <giorgimaisuradze2008@gmail.com>
2025-03-12 18:54:03 +0400

    davaleba nav-bar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {connectToDatabase} from "./services/database.js";
 import bodyParser from "body-parser";
 import {ObjectId} from "mongodb";
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,13 +16,18 @@ app.get('/', async (req, res) => {
     async function run() {
     try {
         const database = await connectToDatabase();
+        const navbar = await database.collection('menus')
+        const cursor = navbar.find();
+        const results = await cursor.toArray();
+        res.json(results)
+        console.log(results)
+
     } catch (error) {
         console.error('Error handling request:', error);
         res.status(500).json({error: 'Internal server error'});
       }
     }
     run().catch(console.dir);
-    res.json({status:"server is running"})
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
